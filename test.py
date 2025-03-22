f9bd0af5ea16762b808a72dac5f245e910a3ba54
James Bushell <jamesrbushell@gmail.com>
2025-03-22 10:36:22 +0000

    Data from csv

--- test.py
+++ test.py
@@ -4,48 +4,44 @@
 # Maybe also
 # https://docs.google.com/spreadsheets/d/0B4IcqARhSzjrUkhRcDVJM2lSaGs/edit?resourcekey=0-snG31ABO-xuU7LWgo1fl8Q&gid=994695874#gid=994695874
 
+import csv
+import sys
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
-timeslots = [
-    "0900",
-    "1000",
-    "1100",
-    "1200",
-    "1300",
-    "1400"
-]
+people = []
+timeslots = []
+data = []
 
-people = [
-    "alice",
-    "bob",
-    "chas",
-    "dave"
-]
+with open(sys.argv[1]) as csvfile:
+    for row in csv.reader(csvfile):
+        if not people:
+            people = row[1:]
+        else:
+            timeslots.append(row[0])
+            data.append(row[1:])
+
+# print(timeslots)
+# print(people)
 
 spare = len(timeslots) - len(people)
 assert spare >= 0
 
 # 0 = available
 # 1 = unavailable
-constraints = [ [1 for _ in range(len(people))] + [0 for _ in range(spare) ] for _ in range(len(timeslots)) ]
+constraints = [
+    [1 for _ in range(len(people))] + [0 for _ in range(spare) ]
+    for _ in range(len(timeslots))
+]
 
-print(constraints)
+# print(constraints)
 
-def set_available(person, timeslot):
-    constraints[timeslots.index(timeslot)][people.index(person)] = 0
+for t in range(len(timeslots)):
+    for p in range(len(people)):
+        if bool(data[t][p]):
+            constraints[t][p] = 0
 
-set_available("alice", "1200")
-set_available("bob",   "0900")
-set_available("bob",   "1100")
-set_available("bob",   "1300")
-set_available("chas",  "1000")
-set_available("chas",  "1300")
-set_available("chas",  "1400")
-set_available("dave",  "0900")
-set_available("dave",  "1200")
-
-print(constraints)
+# print(constraints)
 
 # assert constraints == [
 #     [1,0,1,0,0,0],
@@ -59,9 +55,16 @@ print(constraints)
 cost = np.array(constraints)
 row_indexes, col_indexes = linear_sum_assignment(cost)
 
-for i in range(len(row_indexes)):
-    print(row_indexes[i], col_indexes[i])
-
+# for i in range(len(row_indexes)):
+#     print(row_indexes[i], col_indexes[i])
+#
 for i, col_index in enumerate(col_indexes):
-    print(timeslots[i], people[col_index] if col_index < len(people) else "spare")
+    timeslot = timeslots[i]
+    person = people[col_index] if col_index < len(people) else None
+    if person:
+        bad_choice = constraints[timeslots.index(timeslot)][people.index(person)] != 0
+    else:
+        bad_choice = False
+
+    print(timeslot, person or "--", "!!" if bad_choice else "")
 
